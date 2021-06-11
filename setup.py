2030f839c7b114eb04f4497375064947f6c417a0
Simon Wörpel <simon.woerpel@medienrevolte.de>
2021-06-11 13:51:35 +0200

    Bump version: 0.3.3 → 0.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="mmmeta",
-    version="0.3.3",
+    version="0.4.0",
     description="A simple toolkit for managing local state against remote metadata.",
     long_description=readme(),
     classifiers=[
